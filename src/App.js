5638ef833ae4dcd767450ec763170d47c4d4fbe5
aarni <aarni.haapaniem@aalto.fi>
2022-11-08 00:18:57 +0200

    improved UI, inc. time ranges, menu, animations

--- src/App.js
+++ src/App.js
@@ -1,61 +1,43 @@
 import myData from "./db.json"
+import fakeMenu from "./fakeMenu.json"
+//eslint-disable-next-line
+import getMenu from "./services/getMenu"
 import * as React from 'react';
 import {useState, useEffect} from "react"
 import "./App.css"
 
+/*
+ * 
+ * run instructions: https://github.com/gitname/react-gh-pages
+ * 
+ * TL;DW: 
+ * deploy: 
+ * npm run deploy -- -m "Deploy React app to GitHub Pages"
+ * 
+ * commit (works as normal):
+ * git add .
+ * git commit -m "Configure React app for deployment to GitHub Pages"
+ * git push origin master
+ */
+
 function App() {
-
-  function timeToColor(p) {
-    // B03B3B = long
-    // AB6F39 0 medium
-    // 84BB45 
-    // p = Math.min(Math.max(p, 0), 1)
-    // let r = Math.floor(p * 255).toString(16).padStart(2, "0"),
-    //     g = Math.floor((1-p) * 255).toString(16).padStart(2, "0"),
-    //     b = "00";
-    // return "#" + r + g + b
-
-    if (p < 6) return "#84BB45"
-    if (p < 10) return "#FF7C35"
-    return "#FF3E35"
-  }
-
-  function generateRandomTime(min, max) {
-    let interval = max - min
-    return Math.floor(Math.random() * interval) + min
-  }
-
-  function mapBDEntryToLI(entry) {
-    let maxTime = 20  // anything that is >= this number will show up as red
-    let time = entry.currentTime
-    let pTime = time / maxTime
-    let innerText = [
-        <h2>{
-          `Ravintola ${entry.name}:`
-        }</h2>,
-          <div className="square">
-            <div>
-              <div className="big-text">
-                {time}
-              </div>
-              minuuttia
-            </div>
-          </div>
-    ]
-    return <li style={{backgroundColor: timeToColor(time)}} key={Math.random()*1e9}>
-       {innerText}
-    </li>
-  }
 
   const [data, setData] = useState([])
   const [listItems, setListItems] = useState([])
+  const [menuDB, setMenuDB] = useState([])
   
   useEffect(() => {
+    // let menuData = Object.values(fakeMenu)
+    setMenuDB(fakeMenu)
+    // console.log(fakeMenu);
+    // console.log(menuDB);
     let arrData = Object.values(myData)
-    arrData.map(o => o.currentTime = generateRandomTime(o.minTime, o.maxTime))
+    arrData.map(o => o.currentTimeInterval = generateRandomTime(o.minTime, o.maxTime))
     setData(arrData)
     setListItems(arrData.map(d => mapBDEntryToLI(d)))
-  }, [])
+    // getMenu().then(i => {setMenuDB(i); console.log(i);})
+    // eslint-disable-next-line
+  }, [menuDB])
   
   const handleDefaultSort = () => {
     setListItems(data.map(d => mapBDEntryToLI(d)))
@@ -63,9 +45,61 @@ function App() {
 
   const handleFromSmallestSort = () => {
     let sortedCpy = [...data]  // copy of the array
-    sortedCpy.sort((a, b) => a.currentTime - b.currentTime)
+    sortedCpy.sort((a, b) => a.currentTimeInterval[0] - b.currentTimeInterval[0])
     setListItems(sortedCpy.map(d => mapBDEntryToLI(d)))
   };
+  
+  function timeToColor(t) {
+    if (t < 4) return "#5abf01"
+    if (t < 7) return "#a8d500"
+    if (t < 11) return "#fec901"
+    if (t < 15) return "#f5934c"
+    return "#f0582f"
+  }
+
+  function generateRandomTime(min, max) {
+    let interval = max - min
+    let r1 = Math.floor(Math.random() * interval) + min
+    let r2 = Math.floor(Math.random() * interval) + min
+    return [Math.min(r1, r2), Math.max(r1, r2)]
+  }
+
+  function mapBDEntryToLI(entry) {
+    let time = entry.currentTimeInterval
+    let t1percent = time[0] <= 20 ? time[0]/0.2 : 100
+    let t2percent = time[1] <= 20 ? time[1]/0.2 : 100
+    let innerText = [
+        <h2>
+          {
+            `Ravintola ${entry.name}: `
+          }
+          <br/>
+          <Menu 
+            availableFood={menuDB[entry.id]} 
+          />
+        </h2>,
+          <div className="pie pie1 animate1 no-round" style={{"--p1":t2percent, "--c1":"#8e8e8e"}}>
+            <div className="pie2 animate2 no-round" style={{"--p2":t1percent, "--c2":"#cbcbcb"}}>
+              <div className="square">
+                <div>
+                  <div className="big-text">
+                    {time[0] === time[1] ? time[0] : `${time[0]} - ${time[1]}`}
+                  </div>
+                  minuuttia
+                </div>
+              </div>
+            </div>
+          </div>
+    ]
+    return <li 
+        style={{backgroundColor: timeToColor(1/2 * (time[0] + time[1]))}} 
+        key={Math.floor(Math.random()*1e9)}
+        className="restaurant"
+        // onClick={() => console.log(menuDB)}
+      >
+       {innerText}
+    </li>
+  }
 
   return (
     <div>
@@ -79,12 +113,34 @@ function App() {
           <button onClick={handleFromSmallestSort}>Lyhyin jonotusaika ensin</button>,
         ]}
       />
-      <ul>
+      <ol className="restaurantList">
         {listItems}
-        {/* {data} */}
-      </ul>
+      </ol>
     </div>
   );
+}
+
+const Menu = ({availableFood}) => {
+  const [visible, setVisible] = useState(false)
+
+  
+  if (!visible) return <button onClick={() => setVisible(true)}>Näytä menu</button>
+  
+  if (availableFood === undefined) {
+    console.log("no data")
+    return <>
+      <button onClick={() => setVisible(false)}>Piilota menu</button>
+      No data :(
+    </>
+  }
+
+  let foodValues = Object.values(availableFood)[0]
+  // console.log(foodValues)
+  let foods = foodValues.map(i => <li className="foodmenu-li">{i["title"]}</li>)
+  return <>
+    <button onClick={() => setVisible(false)}>Piilota menu</button>
+    {foods}
+  </>
 }
 
 const Dropdown = ({ trigger, menu }) => {
@@ -100,7 +156,7 @@ const Dropdown = ({ trigger, menu }) => {
         onClick: handleOpen,
       })}
       {open ? (
-        <ol className="menu">
+        <ul className="menu">
           {menu.map((menuItem, index) => (
             <li key={index} className="menu-item">
               {React.cloneElement(menuItem, {
@@ -111,7 +167,7 @@ const Dropdown = ({ trigger, menu }) => {
               })}
             </li>
           ))}
-        </ol>
+        </ul>
       ) : null}
     </div>
   );
---
+++ src/services/getMenu.js
@@ -0,0 +1,21 @@
+// var express = require('express');
+// import { response } from 'express';
+// var cors = require('cors')
+
+// const app = express()
+
+// const corsOptions ={
+//    origin:'*', 
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
+
+// app.use(cors(corsOptions)) // Use this after the variable declaration
+
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+const url = "https://kitchen.kanttiinit.fi/menus?lang=fi&restaurants=7,41,2,3,1,51,64,5,52,59,45,50&days=" + new Date().toISOString().split('T')[0]
+
+export default async function fetchMenu() {
+  const menuJSON = await fetch(proxyUrl + url)
+  return menuJSON.json()
+}
